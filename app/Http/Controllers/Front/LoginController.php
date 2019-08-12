d7ee90040cd9716e5ef5c6262c1466da0e1ead58
Ong Lo <hoangnghiagll@gmail.com>
2019-08-12 10:31:54 +0700

    - Commit

--- app/Http/Controllers/Front/LoginController.php
+++ app/Http/Controllers/Front/LoginController.php
@@ -25,11 +25,6 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-//        dd('ád');
-        if (auth()->guard('employee')->check()) {
-            return redirect()->route('admin.dashboard');
-        }
-
         return view('front.auth.login');
     }
 
@@ -44,16 +39,13 @@ class LoginController extends Controller
     public function login(LoginRequest $request)
     {
         $this->validateLogin($request);
-
         // If the class is using the ThrottlesLogins trait, we can automatically throttle
         // the login attempts for this application. We'll key this by the username and
         // the IP address of the client making these requests into this application.
         if ($this->hasTooManyLoginAttempts($request)) {
             $this->fireLockoutEvent($request);
-
             return $this->sendLockoutResponse($request);
         }
-
         $details = $request->only('email', 'password');
         $details['status'] = 1;
 //        dd($details);
@@ -63,7 +55,6 @@ class LoginController extends Controller
 //            return $this->sendLoginResponse($request);
         }
 //
-
         // If the login attempt was unsuccessful we will increment the number of attempts
         // to login and redirect the user back to the login form. Of course, when this
         // user surpasses their maximum number of attempts they will get locked out.
--- resources/views/front/auth/login.blade.php
+++ resources/views/front/auth/login.blade.php
@@ -1,32 +1,24 @@
-@extends('layouts.front.app')
-
-@section('content')
-    <hr>
-    <!-- Main content -->
-    <section class="container content">
-        <div class="col-md-12">@include('layouts.errors-and-messages')</div>
-        <div class="col-md-4 col-md-offset-4">
-            <h2>Login to your account</h2>
-            <form action="{{ route('login') }}" method="post" class="form-horizontal">
-                {{ csrf_field() }}
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" id="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="Email" autofocus>
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" value="" class="form-control" placeholder="xxxxx">
-                </div>
-                <div class="row">
-                    <button class="btn btn-default btn-block" type="submit">Login now</button>
-                </div>
-            </form>
-            <div class="row">
-                <hr>
-                <a href="{{route('password.request')}}">I forgot my password</a><br>
-                <a href="{{route('register')}}" class="text-center">No account? Register here.</a>
-            </div>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+    <meta charset="UTF-8">
+    <title>Quản lý khách hàng - Đăng nhập</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <link rel="stylesheet" href="./css/custom.css">
+</head>
+<body>
+<div class="wrapper fadeInDown">
+    <div id="formContent">
+        <div class="fadeIn first">
+            <img src="img/logo-1.png" id="icon" alt="User Icon" />
         </div>
-    </section>
-    <!-- /.content -->
-@endsection
+        <form action="{{ route('front.login') }}" method="post" class="form-horizontal">
+            {{ csrf_field() }}
+            <input type="email" id="login" class="fadeIn second" name="email" placeholder="Tên đăng nhập">
+            <input type="password" id="password" class="fadeIn third" name="password" placeholder="Mật khẩu">
+            <input type="submit" class="fadeIn fourth" value="Đăng nhập">
+        </form>
+    </div>
+</div>
+</body>
+</html>
