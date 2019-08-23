1d89dfb5a1cef371c9e60b39cbe10d41b5fd4be0
Ong Lo <hoangnghiagll@gmail.com>
2019-08-23 13:54:48 +0700

    - Commit

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,16 +3,43 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Shop\Campaigns\Campaign;
+use App\Shop\Customer\Customer;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController
 {
     public function index()
     {
-        $breadcumb = [
-            ["name" => "Dashboard", "url" => route("admin.dashboard"), "icon" => "fa fa-dashboard"],
-            ["name" => "Home", "url" => route("admin.dashboard"), "icon" => "fa fa-home"],
-        ];
-        populate_breadcumb($breadcumb);
-        return view('admin.dashboard');
+//        $breadcumb = [
+//            ["name" => "Dashboard", "url" => route("admin.dashboard"), "icon" => "fa fa-dashboard"],
+//            ["name" => "Home", "url" => route("admin.dashboard"), "icon" => "fa fa-home"],
+//        ];
+//        populate_breadcumb($breadcumb);
+//       $campaign = Campaign::where('status',1)->get();
+        $today = Carbon::now()->toDateString();
+
+        $totalCampaign = Campaign::all()->count();
+        $todayCampaign = Campaign::where('time_start', '>=', $today)->where('time_end', '<=', $today)->where('status', true)->get()->count();
+        // khach hang
+        $totalCustomer = Customer::all()->count();
+        $todayCustomer = Customer::where('created_at', Carbon::today())->get()->count();
+        // Tin nhan
+        $totalSms = Customer::where('sms_log_id', '!=', null)->get()->count();
+        $todaySms = Customer::where('created_at', Carbon::today())->where('sms_log_id', '!=', null)->get()->count();
+        //CareSoft
+        $totalCS = Customer::where('care_soft_log_id', '!=', null)->count();
+        $todayCS = Customer::where('created_at', Carbon::today())->where('care_soft_log_id', '!=', null)->get()->count();
+
+        return view('admin.dashboard', ['totalCampaign' => $totalCampaign,
+            'todayCampaign' => $todayCampaign,
+            'totalCustomer' => $totalCustomer,
+            'todayCustomer' => $todayCustomer,
+            'totalSms' => $totalSms,
+            'todaySms' => $todaySms,
+            'totalCS' => $totalCS,
+            'todayCS' => $todayCS,
+        ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,31 +1,9 @@
 @extends('layouts.admin.app')
-
 @section('content')
     <!-- Main content -->
     <section class="content">
-
-        <!-- Default box -->
-    {{--<div class="box">--}}
-    {{--<div class="box-header with-border">--}}
-    {{--<h3 class="box-title">Title</h3>--}}
-
-    {{--<div class="box-tools pull-right">--}}
-    {{--<button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">--}}
-    {{--<i class="fa fa-minus"></i></button>--}}
-    {{--<button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">--}}
-    {{--<i class="fa fa-times"></i></button>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--<div class="box-body">--}}
-    {{--Start creating your amazing application!--}}
-    {{--</div>--}}
-    {{--<!-- /.box-body -->--}}
-    {{--<div class="box-footer">--}}
-    {{--Footer--}}
-    {{--</div>--}}
-    {{--<!-- /.box-footer-->--}}
-    {{--</div>--}}
-    <!-- /.box -->
+        <!-- /.box -->
+        <h1 class="title">Tổng</h1>
         <div class="row">
             <div class="col-lg-3 col-md-6 col-sm-6">
                 <div class="card card-stats">
@@ -33,12 +11,9 @@
                         <div class="card-icon">
                             <i class="fa fa-money"></i>
                         </div>
-                        <p class="card-category">Số lượng đơn hàng</p>
-                        <h3 class="card-title">433
-                            <small>đơn hàng</small>
-                        </h3>
+                        <p class="card-category">Chiến dịch</p>
+                        <h3 class="card-title">{{$totalCampaign}}</h3>
                     </div>
-
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-6">
@@ -48,9 +23,7 @@
                             <i class="fa fa-user"></i>
                         </div>
                         <p class="card-category">Khách hàng</p>
-                        <h3 class="card-title">3223
-                            <small>khách hàng</small></h3>
-
+                        <h3 class="card-title">{{$totalCustomer}}</h3>
                     </div>
                 </div>
             </div>
@@ -60,9 +33,8 @@
                         <div class="card-icon">
                             <i class="fa fa-product-hunt"></i>
                         </div>
-                        <p class="card-category">Sản phẩm</p>
-                        <h3 class="card-title">75
-                                <small>sản phẩm</small></h3>
+                        <p class="card-category">Tin nhắn</p>
+                        <h3 class="card-title">{{$totalSms}}</h3>
                     </div>
                 </div>
             </div>
@@ -72,8 +44,56 @@
                         <div class="card-icon">
                             <i class="fa fa-code-fork"></i>
                         </div>
-                        <p class="card-category">Danh mục</p>
-                        <h3 class="card-title">454 <small>danh mục</small></h3>
+                        <p class="card-category">CareSoft</p>
+                        <h3 class="card-title">{{$totalCS}}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.box -->
+        <h1 class="title">Hôm nay</h1>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-header card-header-warning card-header-icon">
+                        <div class="card-icon">
+                            <i class="fa fa-money"></i>
+                        </div>
+                        <p class="card-category">Chiến dịch</p>
+                        <h3 class="card-title">{{$todayCampaign}}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-header card-header-success card-header-icon">
+                        <div class="card-icon">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <p class="card-category">Khách hàng</p>
+                        <h3 class="card-title">{{$todayCustomer}}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-header card-header-danger card-header-icon">
+                        <div class="card-icon">
+                            <i class="fa fa-product-hunt"></i>
+                        </div>
+                        <p class="card-category">Tin nhắn</p>
+                        <h3 class="card-title">{{$todaySms}}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-header card-header-info card-header-icon">
+                        <div class="card-icon">
+                            <i class="fa fa-code-fork"></i>
+                        </div>
+                        <p class="card-category">CareSoft</p>
+                        <h3 class="card-title">{{$todayCS}}</h3>
                     </div>
                 </div>
             </div>
